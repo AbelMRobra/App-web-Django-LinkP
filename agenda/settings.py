ae1c2f10267fcf6ac477856283fd921429a2e630
Abel Robra <abel_robra_93@hotmail.com>
2020-04-12 19:59:37 -0300

    -

--- agenda/settings.py
+++ agenda/settings.py
@@ -147,7 +147,7 @@ if os.getcwd() == '/app':
 
     #Allow all host headers
     ALLOWED_HOSTS = ['linkpre.herokuapp.com']
-    DEBUG = False
+    DEBUG = True
 
     #Static asset configuration
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
