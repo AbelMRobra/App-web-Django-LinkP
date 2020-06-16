35cea789630fe09b52c3f3b0cb3d551c4b713683
AbelMRobra <abel_robra_93@hotmail.com>
2020-06-16 14:04:55 -0300

    version 1.72

--- finanzas/views.py
+++ finanzas/views.py
@@ -13,42 +13,53 @@ def almacenero(request):
 
     datos = 0
 
+    mensaje = 0
+
     if request.method == 'POST':
 
-        #Trae el proyecto elegido
+        
 
-        proyecto_elegido = request.POST.items()
+        try:
 
-        #Crea los datos del pricing
+            #Trae el proyecto elegido
 
-        datos = []
+            proyecto_elegido = request.POST.items()
 
-        for i in proyecto_elegido:
+            #Crea los datos del pricing
 
-            if i[0] == "proyecto":
-                proyecto = Proyectos.objects.get(id = i[1])
-                presupuesto = Presupuestos.objects.get(proyecto = proyecto)
-                almacenero = Almacenero.objects.get(proyecto = proyecto)
-                datos.append(proyecto)
-                datos.append(presupuesto)
-                datos.append(almacenero)
+            datos = []
 
-                pend_gast = almacenero.pendiente_admin + almacenero.pendiente_comision + presupuesto.saldo_mat + presupuesto.saldo_mo + presupuesto.imprevisto + presupuesto.credito + presupuesto.fdr + almacenero.pendiente_adelantos + almacenero.pendiente_iva_ventas + almacenero.pendiente_iibb_tem
-                prest_cobrar = almacenero.prestamos_proyecto + almacenero.prestamos_otros
-                total_costo = almacenero.cheques_emitidos + almacenero.gastos_fecha + pend_gast + almacenero.Prestamos_dados
-                total_ingresos = prest_cobrar + almacenero.cuotas_cobradas + almacenero.cuotas_a_cobrar + almacenero.ingreso_ventas
-                saldo_caja = almacenero.cuotas_cobradas - almacenero.gastos_fecha - almacenero.Prestamos_dados
-                saldo_proyecto = total_ingresos - total_costo
-                rentabilidad = (saldo_proyecto/total_ingresos)*100
+            for i in proyecto_elegido:
 
-                datos.append((pend_gast, prest_cobrar, total_costo, total_ingresos, rentabilidad, saldo_caja, saldo_proyecto))
+                if i[0] == "proyecto":
+                    proyecto = Proyectos.objects.get(id = i[1])
+                    presupuesto = Presupuestos.objects.get(proyecto = proyecto)
+                    almacenero = Almacenero.objects.get(proyecto = proyecto)
+                    datos.append(proyecto)
+                    datos.append(presupuesto)
+                    datos.append(almacenero)
 
-        proyectos = 0
+                    pend_gast = almacenero.pendiente_admin + almacenero.pendiente_comision + presupuesto.saldo_mat + presupuesto.saldo_mo + presupuesto.imprevisto + presupuesto.credito + presupuesto.fdr + almacenero.pendiente_adelantos + almacenero.pendiente_iva_ventas + almacenero.pendiente_iibb_tem
+                    prest_cobrar = almacenero.prestamos_proyecto + almacenero.prestamos_otros
+                    total_costo = almacenero.cheques_emitidos + almacenero.gastos_fecha + pend_gast + almacenero.Prestamos_dados
+                    total_ingresos = prest_cobrar + almacenero.cuotas_cobradas + almacenero.cuotas_a_cobrar + almacenero.ingreso_ventas
+                    saldo_caja = almacenero.cuotas_cobradas - almacenero.gastos_fecha - almacenero.Prestamos_dados
+                    saldo_proyecto = total_ingresos - total_costo
+                    rentabilidad = (saldo_proyecto/total_ingresos)*100
+
+                    datos.append((pend_gast, prest_cobrar, total_costo, total_ingresos, rentabilidad, saldo_caja, saldo_proyecto))
+
+            proyectos = 0
+
+        except:
+
+            mensaje = 1
 
         
 
     datos = {"proyectos":proyectos,
     'datos':datos,
-    "usd":usd_blue}
+    "usd":usd_blue,
+    "mensaje":mensaje}
 
     return render(request, 'almacenero.html', {"datos":datos} )
