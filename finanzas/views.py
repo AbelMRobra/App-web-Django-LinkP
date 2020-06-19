9c1a497d5a6d29fc2186d633ba51a45624b59d11
AbelMRobra <abel_robra_93@hotmail.com>
2020-06-19 17:08:26 -0300

    version 1.79

--- finanzas/views.py
+++ finanzas/views.py
@@ -33,11 +33,21 @@ def almacenero(request):
 
                 if i[0] == "proyecto":
                     proyecto = Proyectos.objects.get(id = i[1])
+
+
                     presupuesto = Presupuestos.objects.get(proyecto = proyecto)
                     almacenero = Almacenero.objects.get(proyecto = proyecto)
-                    datos.append(proyecto)
-                    datos.append(presupuesto)
-                    datos.append(almacenero)
+
+                    #Aqui calculo el IVA sobre compras
+
+                    iva_compras = (presupuesto.saldo_mat + presupuesto.saldo_mo + presupuesto.credito + presupuesto.fdr + presupuesto.credito)*0.0789209928265611
+
+                    almacenero.pendiente_iva_ventas = iva_compras
+
+                    almacenero.save()
+
+                    #Calculo el resto de las cosas
+                    
 
                     pend_gast = almacenero.pendiente_admin + almacenero.pendiente_comision + presupuesto.saldo_mat + presupuesto.saldo_mo + presupuesto.imprevisto + presupuesto.credito + presupuesto.fdr + almacenero.pendiente_adelantos + almacenero.pendiente_iva_ventas + almacenero.pendiente_iibb_tem
                     prest_cobrar = almacenero.prestamos_proyecto + almacenero.prestamos_otros
@@ -46,6 +56,12 @@ def almacenero(request):
                     saldo_caja = almacenero.cuotas_cobradas - almacenero.gastos_fecha - almacenero.Prestamos_dados
                     saldo_proyecto = total_ingresos - total_costo
                     rentabilidad = (saldo_proyecto/total_ingresos)*100
+
+                    #Cargo todo a datos
+
+                    datos.append(proyecto)
+                    datos.append(presupuesto)
+                    datos.append(almacenero)
 
                     datos.append((pend_gast, prest_cobrar, total_costo, total_ingresos, rentabilidad, saldo_caja, saldo_proyecto))
 
