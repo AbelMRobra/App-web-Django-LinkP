d4d9463b202df1c3687b34eeebe20da2c9f34442
AbelMRobra <abel_robra_93@hotmail.com>
2021-02-09 10:37:12 -0300

    TemplatesAnalisis2

--- agenda/settings.py
+++ agenda/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
