a890020a3883de3670979baaf59d559a9f10d1b4
AbelMRobra <abel_robra_93@hotmail.com>
2021-04-07 16:16:41 -0300

    20210407v3

--- finanzas/views.py
+++ finanzas/views.py
@@ -2266,8 +2266,12 @@ def registro_almacenero(request):
             datos.append((round(margen2_1, 2), round(margen2_2, 2), round((margen2_2/margen2_1 -1 )*100, 2), round((margen2_2 - margen2_1), 2)))
             datos.append((fecha_1, fecha_2))
             datos.append((round((ingresos_total_2/ingresos_total_1 -1 )*100, 3), round((descuento_total_2/descuento_total_1 -1 )*100, 3), round((saldo_caja_total_2/saldo_caja_total_1 -1 )*100, 3), round((pendiente_gastar_total_2/pendiente_gastar_total_1 -1 )*100, 3), round((honorario_2/honorario_1 -1 )*100, 3), round(honorario_2 - honorario_1)))
-            datos_finales = [(ingresos_total_2 - ingresos_total_1), (pendiente_gastar_total_2 - pendiente_gastar_total_1), (saldo_caja_total_2 - saldo_caja_total_1), ((margen1_2 - honorario_2) - (margen1_1 - honorario_1) )]
+            datos_finales = [(ingresos_total_2 - ingresos_total_1), (pendiente_gastar_total_2 - pendiente_gastar_total_1), (saldo_caja_total_2 - saldo_caja_total_1), ((margen1_2 - honorario_2) - (margen1_1 - honorario_1))]
     
+            for v in var_especifica:
+                v[1].append((v[1][0]/((margen1_2 - honorario_2) - (margen1_1 - honorario_1))*100))
+                print(v[1])
+
     return render(request, 'historicoalmacenero.html', {"datos":datos, "mensaje":mensaje, "var_especifica":var_especifica, "datos_finales":datos_finales })
 
 def estudioindice(request, fecha1, fecha2):
