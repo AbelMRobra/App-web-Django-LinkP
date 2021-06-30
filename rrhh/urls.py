a9cd55628b94a24efafb79d4a35b86c643413008
AbelMRobra <abel_robra_93@hotmail.com>
2021-06-30 12:34:04 -0300

    Modificación V 4.0

--- rrhh/urls.py
+++ rrhh/urls.py
@@ -8,9 +8,13 @@ urlpatterns = [
     url(r'^notasdepedido/(?P<id_proyecto>\d+)/(?P<tipo>\d+)/$', login_required(views.notasdepedido), name = 'Notas de pedido'),
     url(r'^notadepedido/(?P<id_nota>\d+)/$', login_required(views.notadepedido), name = 'Nota de pedido'),
     url(r'^crearcorres/$', login_required(views.crearcorrespondencia), name = 'Crear correspondencia'),
-    url(r'^datospersonal/$', login_required(views.datospersonal), name = 'Datos personal'),
+    
     url(r'^apprrhh/$', login_required(views.apprrhh), name = 'App de rrhh'),
     url(r'^editarcorres/(?P<id_nota>\d+)/$', login_required(views.editarcorrespondencia), name = 'Editar correspondencia'),
+    
+    #----------------URL PARA FIANANZAS -----------------------------------------
+    url(r'^datospersonal/$', login_required(views.personal_principal), name = 'Datos personal'),
+    url(r'^perfilpersonal/(?P<id_persona>\d+)/$', login_required(views.personal_perfil), name = 'Perfil personal'),
 
 
 ]
