286ff0cc83316c8c8821196559c6da47e3d3589f
florm2496 <florm2496@gmail.com>
2021-07-05 10:09:30 -0300

    CRUD PAGOS DE RENTA ANTICIPADA -PRIMEROS CAMBIOS vista para agregar, listar y eliminar

--- finanzas/urls.py
+++ finanzas/urls.py
@@ -4,6 +4,9 @@ from . import views
 from .views import DescargarCuentacorriente, DescargarTotalCuentas, PdfPrueba, DescargarResumen, DescargarControlUnidades
 from django.contrib.auth.decorators import login_required
 
+
+
+
 urlpatterns = [
     #----------------URL PARA FIANANZAS -----------------------------------------
 
@@ -41,6 +44,10 @@ urlpatterns = [
     url(r'^calculadora/$', login_required(views.calculadora), name = 'Calculadora'),
 
     url(r'^superavalorcta/(?P<id_cuota>\d+)/$', login_required(views.superarvalorcta), name = 'Cta cliente valor superado'),
+
+
+    path("agregarpago/" ,login_required(views.AgregarPagoRentaAnticipada), name='agregar_pago_renta_anticipada'),
+    path("pagosrentaanticipada/<int:id>" ,login_required(views.ListaPagosRentaAnticipada) ,name='pagosrentaanticipada'),
     
     #----------------URL ADMINISTRACION -----------------------------------------
 
