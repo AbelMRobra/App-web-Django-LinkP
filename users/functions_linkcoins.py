11b3e7c4da4997a253745bf69f22e7ed1bdcc19c
AbelMRobra <64233723+AbelMRobra@users.noreply.github.com>
2021-09-03 14:53:26 -0300

    Linkcoins

--- users/functions_linkcoins.py
+++ users/functions_linkcoins.py
@@ -21,7 +21,7 @@ def estadisticasLinkcoin():
     
     list_mensaje.sort(key = len, reverse=True)
     resultados["mensaje_largo"] = (list_mensaje[0], len(list_mensaje[0]), datosusuario.objects.get(identificacion = con_principal.filter(mensaje = list_mensaje[0]).values_list("usuario_recibe__identificacion", flat=True)[0]))
-    print(resultados["mensaje_largo"])
+
     list_entregas = list(con_principal.values_list("usuario_recibe__identificacion", flat=True))
     resultados["usuario_mas_recibio"] = (datosusuario.objects.get(identificacion = mode(list_entregas)), list_entregas.count(mode(list_entregas)))
 
@@ -29,3 +29,80 @@ def estadisticasLinkcoin():
     resultados["area_querida"] = (mode(list_entregas_areas), list_entregas_areas.count(mode(list_entregas_areas)))
 
     return resultados
+
+
+def mandar_email(mensaje, cabeza, recibe):
+
+    # Establecemos conexion con el servidor smtp de gmail
+    mailServer = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
+    # Construimos el mensaje simple
+    
+    mensaje = mensaje
+
+    mensaje['From']=settings.EMAIL_HOST_USER
+    mensaje['To']= recibe
+    mensaje['Subject']=cabeza
+
+
+    # Envio del mensaje
+
+    mailServer.sendmail(settings.EMAIL_HOST_USER,
+                    recibe,
+                    mensaje.as_string())
+
+
+def email_canje_rrhh(usuario, premio, monedas):
+
+    rrhh = "am@linkinversiones.com.ar"
+
+    #rrhh = "abel.robra.93@gmail.com"
+
+    cabeza = "{} realizo un canje".format(usuario)
+
+    mensaje = MIMEText("""
+                
+Hola!,
+
+{} acaba de canjear Linkcoins, el premio es {} que costó {} monedas.
+
+Podrás visualizarlo en el panel de seguimiento. Cualquier duda, comunicate con el equipo de IT.
+
+Saludos!
+
+-- Link-Help 
+
+
+                    
+                    """.format(usuario, premio, monedas))
+
+    mandar_email(mensaje, cabeza, rrhh)
+
+def email_canje_usuario(email, usuario, premio, monedas):
+
+    rrhh = email
+
+    cabeza = "Realizaste un canje de Linkcoins".format(usuario)
+
+    mensaje = MIMEText("""
+                    
+¡Hola!,
+
+Acabas canjear {}  Linkcoins por el siguiente premio: {}.
+
+El equipo de RRHH te notificará cuando el mismo esté disponible para retirarlo (esta gestión puede tomar hasta 10 días hábiles posteriores a la fecha límite de canje).
+
+Si hubiera algún problema del sistema, comunicate con el equipo de IT para solucionarlo.
+
+Saludos,
+
+-- Link-Help 
+
+                
+                    """.format(monedas, premio))
+
+    mandar_email(mensaje, cabeza, rrhh)
