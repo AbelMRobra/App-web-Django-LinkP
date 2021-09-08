9635f763180c23be42d16d830d8c1b7ee96049ac
AbelMRobra <64233723+AbelMRobra@users.noreply.github.com>
2021-09-08 16:09:08 -0300

    Arreglos

---
+++ presupuestos/wabot.py
@@ -0,0 +1,30 @@
+import json
+import requests
+import datetime
+
+class WABot():
+    def __init__(self, json):
+        self.json = json
+        self.dict_messages = json['messages']
+        self.APIUrl = 'https://api.chat-api.com/instance304624/'
+        self.token = 'cat0hd37m7imu8cz'
+
+    def send_message(self, chatId, text):
+        data = {"chatId" : chatId,
+                "body" : text}
+        answer = self.send_requests('sendMessage', data)
+        return answer
+
+    def send_message_user(self, phone, text):
+        data = {"phone" : phone,
+                "body" : text}
+        answer = self.send_requests('sendMessage', data)
+        return answer
+        
+
+    def send_requests(self, method, data):
+        url = f"{self.APIUrl}{method}?token={self.token}"
+        headers = {'Content-type': 'application/json'}
+        answer = requests.post(url, data=json.dumps(data), headers=headers)
+        return answer.json()
+
