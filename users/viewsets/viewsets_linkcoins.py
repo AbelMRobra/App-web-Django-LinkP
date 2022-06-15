d9ee0f1624cdeb2ec0559bed29dde886e4f8d579
abelmrobra <abel_robra_93@hotmail.com>
2022-06-15 14:50:11 -0300

    Linkcoins: Improve endpoint

--- users/viewsets/viewsets_linkcoins.py
+++ users/viewsets/viewsets_linkcoins.py
@@ -34,7 +34,7 @@ class LinkcoinsViewset(viewsets.GenericViewSet):
 
     @action(detail=True, methods=["GET"])
     def reporte_entrega(self, request, pk=None):
-        quey_canje = EntregaMoneda.objects.filter(usuario_recibe__identificacion=pk).annotate(cantidad=Count('mensaje')).order_by('-fecha')
+        quey_canje = EntregaMoneda.objects.filter(moneda__usuario_portador__identificacion=pk).annotate(cantidad=Count('mensaje')).order_by('-fecha')
         serializer = linkcoins_serializers.EntregasSerializer(quey_canje, many=True)
         response = {'entrega':serializer.data}
         return Response(response, status=status.HTTP_200_OK)
