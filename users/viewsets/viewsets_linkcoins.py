fc5ecac446b303c2bb902932839af5598187ed5b
abelmrobra <abel_robra_93@hotmail.com>
2022-06-15 12:38:45 -0300

    Linkcoins: Change PK in viewset

--- users/viewsets/viewsets_linkcoins.py
+++ users/viewsets/viewsets_linkcoins.py
@@ -11,10 +11,10 @@ class LinkcoinsViewset(viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
 
     @action(detail=True, methods=["GET"])
-    def reporte(self, request, ident=None):
-        quey_canje = CanjeMonedas.objects.filter(usuario__identificacion=ident).count()
-        monedas_generadas = MonedaLink.objects.filter(usuario_portador__identificacion=ident).count()
-        monedas_recibidas = EntregaMoneda.objects.filter(usuario_recibe__identificacion=ident).count()
+    def reporte(self, request, pk=None):
+        quey_canje = CanjeMonedas.objects.filter(usuario__identificacion=pk).count()
+        monedas_generadas = MonedaLink.objects.filter(usuario_portador__identificacion=pk).count()
+        monedas_recibidas = EntregaMoneda.objects.filter(usuario_recibe__identificacion=pk).count()
         monedas_canjeadas = sum(np.array(quey_canje.values_list('monedas', flat=True)))
         canjes_pendientes = quey_canje.filter(entregado='NO').count()
         ultimo_canje = quey_canje.order_by('-fecha').first()
